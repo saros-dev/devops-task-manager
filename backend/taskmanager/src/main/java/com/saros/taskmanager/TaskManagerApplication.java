d2b567e7c2a42f7dc6f2e8407408d15ea78d9c61
saros-dev <shojaiisaros@yahoo.ca>
2026-06-10 03:04:17 +0330

    fix: add datasource env vars

--- backend/taskmanager/src/main/java/com/saros/taskmanager/TaskManagerApplication.java
+++ backend/taskmanager/src/main/java/com/saros/taskmanager/TaskManagerApplication.java
@@ -3,7 +3,7 @@ package com.saros.taskmanager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+@SpringBootApplication
 public class TaskManagerApplication {
 
     public static void main(String[] args) {
